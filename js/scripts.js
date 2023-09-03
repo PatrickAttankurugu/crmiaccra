725a4206535e398fb9f5a196f29064d8774ad49e
Patrick Attankurugu <patricka.azuma@gmail.com>
2023-09-03 01:34:16 +0000

    added staff facebook pages

--- js/scripts.js
+++ js/scripts.js
@@ -40,16 +40,47 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     }
   
-    // New functionality for hover effect on pastor's image
-    var pastorImage = document.querySelector('.pastor-image img');
-    if (pastorImage) {
-      pastorImage.addEventListener('mouseover', function () {
-        this.classList.add('hovered');
-      });
-  
-      pastorImage.addEventListener('mouseout', function () {
-        this.classList.remove('hovered');
+    // Text animation for hero title
+    var heroTitle = document.querySelector('#hero-image .animated-text');
+    if (heroTitle) {
+      let textArray = heroTitle.textContent.split("");
+      heroTitle.innerHTML = "";
+      textArray.forEach((char, index) => {
+        heroTitle.innerHTML += `<span style="transition-delay:${index * 0.05}s">${char}</span>`;
       });
     }
+  
+    // Parallax effect for hero background
+    var heroSection = document.getElementById('hero-image');
+    if (heroSection) {
+      window.addEventListener('scroll', function () {
+        var offset = window.pageYOffset;
+        heroSection.style.backgroundPositionY = offset * 0.7 + 'px';
+      });
+    }
+
+    // Smooth scrolling to 'know-us' section when 'Our Mission' button is clicked
+  var missionBtn = document.getElementById('mission-button');
+  if (missionBtn) {
+    missionBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      var knowUsSection = document.getElementById('know-us');
+      knowUsSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  // Dynamic typing functionality
+  var typeString = 'Loving God and Loving People';
+  var typeElement = document.querySelector('#know-us h3');
+  if (typeElement) {
+    let i = 0;
+    var typing = setInterval(() => {
+      typeElement.textContent += typeString.charAt(i);
+      i++;
+      if (i === typeString.length) {
+        clearInterval(typing);
+      }
+    }, 200);
+  }
   });
   
