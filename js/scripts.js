8da8bc6c13750a6fa05e7a9d9aa17b8fb812da0e
Patrick Attankurugu <patricka.azuma@gmail.com>
2023-09-07 15:19:26 +0000

    added mission

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,65 @@
+// JavaScript Document for Church Website
 document.addEventListener('DOMContentLoaded', function () {
-
-    // Existing functionality for event cards
-    var eventCards = document.querySelectorAll('.event');
-    eventCards.forEach(function (card) {
-      card.addEventListener('click', function () {
-        var eventTitle = this.querySelector('.card-title').innerText;
-        alert('You clicked on ' + eventTitle);
-      });
+  
+  // Functionality for clicking on event cards
+  var eventCards = document.querySelectorAll('.event');
+  eventCards.forEach(function (card) {
+    card.addEventListener('click', function () {
+      var eventTitle = this.querySelector('.card-title').innerText;
+      alert('You clicked on ' + eventTitle);
     });
-  
-    // Functionality for staff-member hover and click effects
-    var staffMembers = document.querySelectorAll('.staff-member');
-    staffMembers.forEach((member) => {
-      
-      // Add hover effect
-      member.addEventListener('mouseover', function () {
-        this.classList.add('hovered');
-      });
-      
-      // Remove hover effect
-      member.addEventListener('mouseout', function () {
-        this.classList.remove('hovered');
-      });
-      
-      // Add click effect
-      member.addEventListener('click', function () {
-        this.classList.toggle('selected');
-      });
-      
-    });
-  
-    // Smooth scrolling to staff-section when 'Our Leadership' button is clicked
-    var leadershipBtn = document.querySelector('.btn');
-    if (leadershipBtn) {
-      leadershipBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        var staffSection = document.getElementById('staff-section');
-        staffSection.scrollIntoView({ behavior: 'smooth' });
-      });
-    }
-  
-    // Text animation for hero title
-    var heroTitle = document.querySelector('#hero-image .animated-text');
-    if (heroTitle) {
-      let textArray = heroTitle.textContent.split("");
-      heroTitle.innerHTML = "";
-      textArray.forEach((char, index) => {
-        heroTitle.innerHTML += `<span style="transition-delay:${index * 0.05}s">${char}</span>`;
-      });
-    }
-  
-    // Parallax effect for hero background
-    var heroSection = document.getElementById('hero-image');
-    if (heroSection) {
-      window.addEventListener('scroll', function () {
-        var offset = window.pageYOffset;
-        heroSection.style.backgroundPositionY = offset * 0.7 + 'px';
-      });
-    }
+  });
 
-    // Smooth scrolling to 'know-us' section when 'Our Mission' button is clicked
-  var missionBtn = document.getElementById('mission-button');
-  if (missionBtn) {
-    missionBtn.addEventListener('click', function (e) {
+  // Functionality for hovering and clicking on staff members
+  var staffMembers = document.querySelectorAll('.staff-member');
+  staffMembers.forEach((member) => {
+    member.addEventListener('mouseover', function () {
+      this.classList.add('hovered');
+    });
+    member.addEventListener('mouseout', function () {
+      this.classList.remove('hovered');
+    });
+    member.addEventListener('click', function () {
+      this.classList.toggle('selected');
+    });
+  });
+
+  // Smooth scrolling to 'staff-section' when 'Our Leadership' button is clicked
+  var leadershipBtn = document.querySelector('.btn');
+  if (leadershipBtn) {
+    leadershipBtn.addEventListener('click', function (e) {
       e.preventDefault();
-      var knowUsSection = document.getElementById('know-us');
-      knowUsSection.scrollIntoView({ behavior: 'smooth' });
+      var staffSection = document.getElementById('staff-section');
+      staffSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+  
+  // Smooth scrolling to 'mission' section when 'Tell Us' button is clicked
+  var tellUsBtn = document.getElementById('tell-us-button');
+  if (tellUsBtn) {
+    tellUsBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      var missionSection = document.getElementById('mission');
+      missionSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  // Text Animation for the Hero Title
+  var heroTitle = document.querySelector('#hero-image .animated-text');
+  if (heroTitle) {
+    let textArray = heroTitle.textContent.split("");
+    heroTitle.innerHTML = "";
+    textArray.forEach((char, index) => {
+      heroTitle.innerHTML += `<span style="transition-delay:${index * 0.05}s">${char}</span>`;
+    });
+  }
+
+  // Parallax Effect for Hero Background
+  var heroSection = document.getElementById('hero-image');
+  if (heroSection) {
+    window.addEventListener('scroll', function () {
+      var offset = window.pageYOffset;
+      heroSection.style.backgroundPositionY = offset * 0.7 + 'px';
     });
   }
 
@@ -82,5 +76,5 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }, 200);
   }
-  });
-  
+
+});
